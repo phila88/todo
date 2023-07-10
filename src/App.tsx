86285ad6bf5e35cccdda378b13278588e4245bb7
Phillip Arthur <pvarthur@pm.me>
2023-07-09 20:37:39 -0400

    First pass at creating tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [tasks, setTasks] = useState<string[]>([]);
+  const todoList = tasks.map((t: string) => <p>{t}</p>);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+    const newTask = formJson['task'].toString();
+
+    if (newTask !== '') setTasks([...tasks, newTask]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="py-2 px-3 sm:px-0 max-w-lg m-auto space-y-4">
+      <div className="text-center font-semibold text-2xl">Todo App</div>
+      <form onSubmit={handleSubmit} className="space-x-1">
+        <label>
+          Task:{' '}
+          <input
+            type="text"
+            name="task"
+            className="p-1 border-2 border-black rounded"
+          />
+        </label>
+        <button type="submit" className="p-1.5 bg-yellow-500 rounded">
+          Create Task
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </form>
+      <ul className="space-y-2">{todoList}</ul>
+    </div>
   );
 }
 
