b1e7e2a7692ab0864f6ec1bf4e346cb3e76dd836
Phillip Arthur <pvarthur@pm.me>
2023-07-20 21:21:38 -0400

    Update checkbox styling

--- src/components/task.tsx
+++ src/components/task.tsx
@@ -1,3 +1,6 @@
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { CheckIcon } from '@radix-ui/react-icons';
+
 export type TaskData = {
   id: number;
   name: string;
@@ -12,45 +15,32 @@ type Props = {
 
 export const Task = ({ data, onChange, onClose }: Props) => (
   <li className="group flex items-center space-x-2 rounded-md p-2 hover:bg-blue-dark/75">
-    {/* TODO: Fix checkbox styling */}
-    {/* <div className="relative flex items-center">
-        <input
-          type="checkbox"
-          name="task-check"
-          id={t.id}
-          className="h-5 w-5 appearance-none rounded bg-white checked:bg-mint hover:checked:brightness-105 active:brightness-95"
-        />
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={3}
-          stroke="currentColor"
-          className="absolute inset-0 h-5 w-5"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M4.5 12.75l6 6 9-13.5"
-          />
-        </svg>
-      </div> */}
-    <input
-      type="checkbox"
-      name="task-check"
-      id="task-check"
-      className="peer h-6 w-6 accent-mint"
-      checked={data.complete ?? false}
-      onChange={onChange}
-    />
-    <span className="flex-1 text-lg font-semibold peer-checked:text-white/60">
+    <Checkbox.Root
+      checked={data.complete}
+      onCheckedChange={onChange}
+      id="c1"
+      className={`flex h-5 w-5 appearance-none items-center justify-center rounded hover:brightness-105 active:brightness-95 ${
+        data.complete ? 'bg-mint' : 'bg-white'
+      } outline-blue checked:bg-white`}
+    >
+      <Checkbox.Indicator className="text-white">
+        <CheckIcon className="h-5 w-5" />
+      </Checkbox.Indicator>
+    </Checkbox.Root>
+    <label
+      htmlFor="c1"
+      className={`flex-1 text-lg font-medium ${
+        data.complete ? 'opacity-75' : ''
+      }`}
+    >
       {data.complete ? <del>{data.name}</del> : data.name}
-    </span>
+    </label>
+
     <button
       onClick={onClose}
-      className="hidden h-6 w-6 rotate-45 rounded-full bg-white font-bold text-blue-dark group-hover:block"
+      className="hidden h-5 w-5 items-center justify-center rounded-full bg-white text-2xl font-medium leading-none text-blue group-hover:flex"
     >
-      +
+      {'\u00d7'}
     </button>
   </li>
 );
